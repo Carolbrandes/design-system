0c8988c8a2ea28d9f5a01d7bcfcc087423b4cb76
carol <carolbrandes23@gmail.com>
2025-04-16 08:10:50 -0300

    feat/story: Button

--- packages/docs/eslint.config.js
+++ packages/docs/eslint.config.js
@@ -1,13 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: ["@ignite-ui/eslint-config"],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
