4b33a5a29634d4112f1457fc7be0f958d30e0501
carol <carolbrandes23@gmail.com>
2025-04-24 08:23:25 -0300

    feat/Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,23 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+// config global
+export default {
+    title: 'Data display/Avatar',
+    component: Avatar,
+    args: {
+        src: "https://github.com/diego3g.png",
+        alt: "Diego Fernandes"
+    }
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+    args: {
+        src: undefined
+    }
+}
+
+
+
