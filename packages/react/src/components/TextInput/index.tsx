232099a1cc63d21434eb24edfd5aea723fcb6eab
carol <carolina.brandes@assurant.com>
2025-05-02 10:34:49 -0300

    fix

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,18 +1,20 @@
 import { ComponentProps } from "react";
 import { Input, Prefix, TextInputContainer } from "./styles";
 
-export interface TextInputProps extends ComponentProps<typeof TextInput> {
+// Extend from the Input component's props instead
+export interface TextInputProps extends ComponentProps<typeof Input> {
     disabled?: boolean;
     placeholder?: string;
     prefix?: string;
 }
 
 export function TextInput({ prefix, ...props }: TextInputProps) {
-    return <TextInputContainer>
-        {!!prefix && <Prefix>{prefix}</Prefix>}
-        <Input {...props} />
-    </TextInputContainer>
+    return (
+        <TextInputContainer>
+            {!!prefix && <Prefix>{prefix}</Prefix>}
+            <Input {...props} />
+        </TextInputContainer>
+    )
 }
 
-
-TextInput.displayName = 'TextInput'
+TextInput.displayName = 'TextInput';
